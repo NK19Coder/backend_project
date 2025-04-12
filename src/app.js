a162cd92be7e1e074e5768155122fb4b29443dc4
Nishant <43836369+NK19Coder@users.noreply.github.com>
2025-04-13 00:01:58 +0530

    custom API handling

---
+++ src/app.js
@@ -0,0 +1,22 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors"; 
+
+const app = express();
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credential: true 
+})) // for middleware  
+
+app.use(express.json({
+    limit:"16kb"  
+}))    // for taking object 
+
+app.use(express.urlencoded({extended:true, limit:"16kb"})) //for taking url
+app.use(express.static("public")) //for taking static files
+   
+app.use(cookieParser()) 
+ 
+ 
+export { app }     
